7abd75f23b7b8f0146e8034327c9b7aaec5e66ae
jcsamoyed <aruby5223@gmail.com>
2023-12-21 00:51:17 +0800

    feat: 介面定義

--- src/vehicleInterface.ts
+++ src/vehicleInterface.ts
@@ -2,7 +2,7 @@
  * 車輛介面已被移除，你需要自己定義一個新的車輛介面。
  * 車輛介面應該包含 brand、model 和 year 三個屬性，分別為字串型別和數字型別。
  */
-
+interface IVehicle { brand: string, model: string, year: number }
 /**
  * 獲取車輛的詳細資訊
  * @param vehicle - 車輛物件，應該包含 brand、model 和 year 三個屬性
@@ -11,6 +11,7 @@
  * 輸入: { brand: 'Toyota', model: 'Corolla', year: 2020 }
  * 輸出: 'Toyota Corolla (2020)'
  */
-export function vehicleInfo(vehicle: /* 你的車輛介面 */): string {
+export function vehicleInfo(vehicle: IVehicle): string {
     // 在此實現函式
+    return `${vehicle.brand} ${vehicle.model} (${vehicle.year})`
 }
